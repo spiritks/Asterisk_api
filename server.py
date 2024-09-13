ff878756f646bff93c22d11f66521f32601f949f
Pavlo Pototskyi <spirit.kherson@gmail.com>
2024-09-13 13:24:06 +0200

    1

--- server.py
+++ server.py
@@ -22,8 +22,8 @@ def attended_transfer():
         'Status',
     )
     response = client.send_action(action_status)
-    if not response.response:
-        return "got no response"
+    if  response.response:
+        return response
     channels = [channel.get_header('Channel') for channel in response.response if internal_number in channel.get_header('Channel')]
 
     if not channels:
