59446bbb7cfa181c07b599fa2d09ca854bea9005
Pavlo Pototskyi <spirit.kherson@gmail.com>
2024-09-13 14:04:20 +0200

    2

--- server.py
+++ server.py
@@ -21,7 +21,9 @@ def attended_transfer():
     action_status = SimpleAction(
         'Status',
     )
-    response = client.send_action(action_status).response
+    response = client.send_action(action_status)
+    if not response.response:
+        return "No response received"
     channels=[]
     for key in response.keys():
         if key.startswith("Channel"):
