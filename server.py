38c761fd5e303cfc286da48963244a610e71259e
Pavlo Pototskyi <spirit.kherson@gmail.com>
2024-09-13 16:04:01 +0200

    1

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def attended_transfer():
     action_status = SimpleAction(
         'Status',
     )
-    response = vars(client.send_action(action_status))
+    response = vars(client.send_action(action_status).response)
     return jsonify(response)
     # if  response.response:
     #     return str(type(response.response))
